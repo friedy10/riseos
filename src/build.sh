1c3f2a1c96a9f4e043d5c7f2c9e1c1e7bdc46883
professor <friedrichdoku@gmail.com>
2020-07-19 23:12:02 -0500

    minor changes

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,4 @@
-/home/professor/Documents/riseos/barebones-toolchain/setenv.sh
 nasm -felf32 boot.s -o boot.o
-i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -ffreestanding -nostdlib -fno-use-linker-plugin -g -T linker.ld boot.o kernel.o -o rise.elf -lgcc
+gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+gcc -ffreestanding -nostdlib -fno-use-linker-plugin -g -T linker.ld boot.o kernel.o -o rise.elf -lgcc
 
