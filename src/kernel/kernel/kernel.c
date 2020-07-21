dbc2b1685c344a1f5776c543c981f7bbf8aca7f9
professor <friedrichdoku@gmail.com>
2020-07-21 10:11:55 -0500

    minor change

--- src/kernel/kernel/kernel.c
+++ src/kernel/kernel/kernel.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
+#include <kernel/pmm.h>
 
 void kernel_main(void) {
+    init_bitmap();
 	terminal_initialize();
 	printf("Hello, kernel World!\n");
+
 }
