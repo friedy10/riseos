a2e3294a5c3dbd76620035fc4438c9441c6b7e86
professor <friedrichdoku@gmail.com>
2020-07-21 15:45:24 -0500

    Minor change

--- src/kernel/kernel/kernel.c
+++ src/kernel/kernel/kernel.c
@@ -7,5 +7,9 @@ void kernel_main(void) {
     init_bitmap();
     terminal_initialize();
     printf("Hello, kernel World!\n");
-
+    printf("Testing memeory manager\n");
+    for(int i = 0; i < 100; i++){
+        physaddr_t frame = kalloc_page_frame();
+        printf("Made frame at: %x", frame);
+    }
 }
